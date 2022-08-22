4306e5db0943b933ca8af3ccd5531921b1c752b7
rishabh jain <ishujain415@gmail.com>
2022-08-22 11:20:58 +0530

    make it better

--- contact.php
+++ contact.php
@@ -1,28 +1,21 @@
-<?php 
 
-	//print_r($_POST); 
-	//die();
-	$servername = "localhost:3307";
-	$username = "root";
-	$password = "";
-	$dbname = "breza";
 
-	$name = $_POST['name'];
-	$number = $_POST['number'];
-	$email = $_POST['email'];
-	$comments = $_POST['comments'];	
 
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	if($conn-> connect_error){
-		die('connection failed : '.$conn->connect_error);
-	}else{
-		$stmt = $conn->prepare("insert into contact(name, number, email, message) values(?,?,?,?)");
-		$stmt->bind_param("ssss", $name , $number , $email , $comments);
-		$stmt->execute();
-		echo "we will reach you soon Thank you!";
-		$stmt->close();
-		$conn->close();
-	} 
+<?php
+//get data from form  
 
-	
+$name = $_POST['name'];
+$nuber = $_POST['number'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+$to = "1708contact@gmail.com";
+$subject = "Mail From breza";
+$txt ="Name = ". $name . " \r\nNumber = ". $number . "\r\nEmail = " . $email . "\r\nMessage =" . $message;
+$headers = "From: noreply@breza.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:index.html");
 ?>
